5ca4076aad2c37a5b862bfd537f378537c02dd9e
KallaPavan333 <kallaanand956@gmail.com>
2026-02-14 14:57:30 +0530

    second commit

--- src/main/java/cse_jenkins/jenkins/App.java
+++ src/main/java/cse_jenkins/jenkins/App.java
@@ -5,6 +5,6 @@ package cse_jenkins.jenkins;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to Jenkins!");
     }
 }
